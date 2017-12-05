2f901eb93aff62713e85c0facecc089d763c0730
Rob-Samalonis <samalonis@yahoo.com>
2017-12-04 22:34:28 -0500

    added email and location

--- src/aside.js
+++ src/aside.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Me from './me.jpg';
+import Location from './location.png';
+import Mail from './mail.png'
 import './aside.css'
 // Make it like the aside on this page http://jonbloomer.com.au/
 class Aside extends Component {
   render() {
     return (
       <div>
-          <img className="me" src={Me} alt="me" />
-        <h4>This is my aside</h4>
+        <img className="me" src={Me} alt="me" />
+        <h2>Robert Samalonis</h2>
+        <h4>Software Engineer - Elsevier</h4>
+        <hr />
+        <span className="address"><img className="location" src={Location} alt="location" />Philadelphia, PA</span>
+        <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top" className= "email"><img className="mail" src={Mail} alt="mail" />r.samalonis@elsevier.com</a>
       </div>
     );
   }
